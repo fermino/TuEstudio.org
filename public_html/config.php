471e251d10f78e0b1c8e0993651610a3f078a614
Fermin Olaiz <ferminolaiz@gmail.com>
2017-09-30 19:20:43 -0300

    Primera prueba de config.php

---
+++ public_html/config.php
@@ -0,0 +1,45 @@
+<?php
+	// Do not touch this!
+	if(defined('SITE_MODE'))
+		exit;
+
+	/**
+	 * SITE_MODE:
+	 *	0 = development
+	 *	1 = production
+	 *	2 = test
+	 */
+
+	define('SITE_MODE', 0);
+
+	$cfg = array
+	(
+		'project_path'	=> '..',
+		'dbs'			=> array
+		(
+			'development'	=> array
+			(
+				'pdo_driver'	=> 'mysql',
+				'username'		=> 'username',
+				'password'		=> 'password',
+				'socket'		=> '127.0.0.1',	// Either unix socket or host[:port]
+				'database'		=> 'database_name_dev'
+			),
+			'production'	=> array
+			(
+				'pdo_driver'	=> 'mysql',
+				'username'		=> 'username',
+				'password'		=> 'password',
+				'socket'		=> '127.0.0.1',
+				'database'		=> 'database_name_prod'
+			),
+			'test'			=> array
+			(
+				'pdo_driver'	=> 'mysql',
+				'username'		=> 'username',
+				'password'		=> 'password',
+				'socket'		=> '127.0.0.1',
+				'database'		=> 'database_name_test'
+			)
+		)
+	);
