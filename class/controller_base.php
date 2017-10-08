33c9b0d79f9510c4579cc482f13e48823e879cf7
Fermin Olaiz <ferminolaiz@gmail.com>
2017-10-08 18:28:29 -0300

    Ejecutamos el controlador antes de cargar la vista (fix)

--- class/controller_base.php
+++ class/controller_base.php
@@ -9,10 +9,15 @@
 		protected $unique_view = true;
 		protected $view = null;
 
-		final public function handleRequest(string $http_method, array $get_params) : int
+		final public function handleRequest(string $http_method, array $environment) : int
 		{
 			if(method_exists($this, strtolower($http_method)))
 			{
+				$response = $this->{$http_method}($environment);
+
+				if(is_int($response) && 200 !== $response && 0 !== $response)
+					return $response;
+
 				if($this->unique_view)
 				{
 					$view_name = get_class($this);
@@ -27,7 +32,7 @@
 					{
 						if($this->view->parse())
 						{
-							if($this->view->display($get_params))
+							if($this->view->display(is_array($response) ? $response : $environment))
 								return 0;
 							else
 							{
@@ -47,8 +52,6 @@
 						return 500;
 					}
 				}
-
-				return $this->{$http_method}($get_params);
 			}
 
 			// Log error
