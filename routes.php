f8787b17b165ecb1c2ebb9f1b45ea1094c181133
Fermin Olaiz <ferminolaiz@gmail.com>
2017-10-12 12:09:00 -0300

    Limpiamos las rutas

--- routes.php
+++ routes.php
@@ -1,13 +1,5 @@
 <?php
 	$routes = 
 	[
-		'/users'	=>	'users',
-		//'cosaboni'	=> null,
-		'/users27'	=>	['users27'],
-		'/dates'	=>	['datescontrolador', ['get', 'post', 'patch']],
-		'/admin'	=>	
-		[
-			'/'	=>	['usuariosxd', ['put', 'GET']],
-			'/x' => 'y'
-		]
+		'/'			=> 'index'
 	];
