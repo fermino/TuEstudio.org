5088a9daff9bb68a5c2c734acfb579d297d85a0d
Fermin Olaiz <ferminolaiz@gmail.com>
2017-10-31 21:14:24 -0300

    Cambiamos el nombre State a Province
    * Y cambiamos el largo máximo de name a 255

--- models/province.php
+++ models/province.php
@@ -1,23 +1,23 @@
 <?php
-	class State extends ActiveRecord\Model
+	class Province extends ActiveRecord\Model
 	{
 		/**
-		 *	CREATE TABLE `states` (
+		 *	CREATE TABLE `provinces` (
 		 *	  `id` int(11) NOT NULL,
-		 *	  `name` varchar(32) NOT NULL
+		 *	  `name` varchar(255) NOT NULL
 		 *	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 		 *	
-		 *	ALTER TABLE `states`
+		 *	ALTER TABLE `provinces`
 		 *	  ADD PRIMARY KEY (`id`),
 		 *	  ADD UNIQUE KEY `name` (`name`);
 		 *	
-		 *	ALTER TABLE `states`
+		 *	ALTER TABLE `provinces`
 		 *	  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 		 */
 
 		public static $validates_length_of =
 		[
-			['name',	'within' => [1, 32]]
+			['name',	'within' => [1, 255]]
 		];
 
 		public static $validates_uniqueness_of =
