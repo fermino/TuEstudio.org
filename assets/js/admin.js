bad16d87c79533fe26e430aa4513b74770a10cd2
Fermin Olaiz <ferminolaiz@gmail.com>
2017-11-02 12:35:55 -0300

    Agregamos la creación y edición de provincias

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,20 @@
 $(function()
 {
 	$('#admin-' + $('#admin-nav').attr('data-active')).addClass('active');
+
+	$('#createEditModal').on('show.bs.modal', function(event)
+	{
+		var actionMessage = $(event.relatedTarget).data('action');
+
+		$(this).find('h4 #createEditModal-title').text(actionMessage);
+		$(this).find('button #createEditModal-title').text(actionMessage);
+
+		var button = $(event.relatedTarget);
+
+		$('#createEdit-id-label').text(button.data('id'));
+		$('#createEdit-id').val(button.data('id'));
+
+		$('#createEdit-name').val(button.data('name'));
+		$('#createEdit-name').focus();
+	});
 });
--- controllers/admin_provinces.php
+++ controllers/admin_provinces.php
@@ -6,12 +6,15 @@
 		public function get($search = null)
 		{
 			if(null === $search)
-				$provinces = Province::all();
+				$provinces = Province::all(
+				[
+					'order'		=> 'name ASC'
+				]);
 			else
 				$provinces = Province::all(
 				[
 					'conditions'	=> ['name LIKE ?', '%' . $search . '%'],
-					'order'			=> ['name DESC']
+					'order'			=> 'name ASC'
 				]);
 
 			return ['provinces' => $provinces, 'search' => $search];
@@ -19,7 +22,40 @@
 
 		public function post()
 		{
-			if(!empty($_POST['search']))
+			if(!empty($_POST['id']) && '-' === $_POST['id'] && !empty($_POST['name']))
+			{
+				$province = new Province;
+				$province->name = $_POST['name'];
+
+				if($province->save())
+					return '/admin/provinces#' . $province->id;
+
+				return '/admin/provinces';
+			}
+			else if(!empty($_POST['id']) && '-' !== $_POST['id'] && !empty($_POST['name']))
+			{
+				try
+				{
+					$province = Province::find($_POST['id']);
+
+					if(null !== $province)
+					{
+						$province->name = $_POST['name'];
+
+						if($province->save())
+							return '/admin/provinces#' . $_POST['id'];
+						else
+							return '/admin/provinces?error=unique&col=name&pcol=nombre&val=' . urlencode($_POST['name']);
+					}
+
+					return '/admin/provinces#' . $_POST['id'];
+				}
+				catch(ActiveRecord\RecordNotFound $e)
+				{
+					return '/admin/provinces';
+				}
+			}
+			else if(!empty($_POST['search']))
 				return '/admin/provinces/' . urlencode($_POST['search']);
 			else
 				return '/admin/provinces';
