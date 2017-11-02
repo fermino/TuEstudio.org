801fe6cae9b4d298880d9630c661099417a9d5a1
Fermin Olaiz <ferminolaiz@gmail.com>
2017-11-02 12:33:25 -0300

    Agregamos iziToast para gestión de errores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 $(function()
 {
+	if(null != error)
+		iziToast.error({title: 'Error', message: 'La columna ' + pcol +' debe ser única'});
+
 	$('.button-link').each(function(i, button)
 	{
 		$(button).click(function()
